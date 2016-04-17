25f8b107820454ab2493e4c463bcfe2196ab08eb
NYamaji <yamaji1012@gmail.com>
2016-04-18 01:33:58 +0900

    Add project files.

---
+++ src/DotNetCoreWebApi/Repository/YbRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace DotNetCoreWebApi.Repository
+{
+
+    public class YbRepository
+    {
+        private readonly string connectionString = "Data Source=localhost;Initial Catalog=Wel81;User ID=sa;Password=ymj123!";
+        private DbConnection connection;
+
+        public YbRepository()
+        {
+            this.connection = new SqlConnection(connectionString);
+        }
+
+        public IEnumerable<Yb001> GetYb001ByJno(string jno)
+        {
+            string query = "SELECT * FROM yb001 WHERE 整理番号 = @jno";
+            return this.connection.Query<Yb001>(query,new { jno = jno});
+        }
+    }
+}
---
+++ src/DotNetCoreWebApi/Yb001.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetCoreWebApi.Repository
+{
+    public class Yb001
+    {
+        public string 整理番号 { get; set; }
+
+        public int 接種名称区分 { get; set; }
+
+        public string 接種名称 { get; set; }
+
+        public int 接種区分 { get; set; }
+    }
+}
